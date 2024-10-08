8cfa9bde2c3e39fdd2028135a06828e23881d460
DarKrone <Brener-2000@mail.ru>
2024-10-09 01:45:43 +0400

    ArrayBencodeTest

--- src/Program.cs
+++ src/Program.cs
@@ -22,34 +22,8 @@ if (command == "decode")
     }
     else if (encodedValue[0] == 'l')
     {
-        List<object> array = new List<object>();
-        string tempEncodeValue = encodedValue.Substring(1);
-
-        while (tempEncodeValue != "e")
-        {
-            if (Char.IsDigit(tempEncodeValue[0]))
-            {
-                var colonIndex = tempEncodeValue.IndexOf(':');
-                var strLength = int.Parse(tempEncodeValue[..colonIndex]);
-
-                string tempString = tempEncodeValue.Substring(0, colonIndex + strLength + 1);
-                array.Add(StringBencode(tempString));
-
-                tempEncodeValue = tempEncodeValue.Substring(colonIndex + strLength + 1);
-            }
-            else if (tempEncodeValue[0] == 'i')
-            {
-                var endIndex = tempEncodeValue.IndexOf('e');
-                var numberLength = int.Parse(tempEncodeValue[1..endIndex]);
-
-                string tempString = tempEncodeValue.Substring(0, endIndex + 1);
-
-                array.Add(IntegerBencode(tempString));
-
-                tempEncodeValue = tempEncodeValue.Substring(endIndex + 1);
-            }
-        }
-        Console.WriteLine(JsonSerializer.Serialize(array));
+        
+        Console.WriteLine(JsonSerializer.Serialize(ArrayBencode(encodedValue)));
     }
     else
     {
@@ -95,4 +69,43 @@ long IntegerBencode(string encodedValue)
     {
         throw new InvalidOperationException("Missing end of output (e)");
     }
+}
+
+List<object> ArrayBencode(string encodedValue)
+{
+    List<object> array = new List<object>();
+    string tempEncodeValue = encodedValue.Substring(1);
+    Console.WriteLine(encodedValue);
+    while (tempEncodeValue != "e" || tempEncodeValue.Length == 0)
+    {
+        if (Char.IsDigit(tempEncodeValue[0]))
+        {
+            var colonIndex = tempEncodeValue.IndexOf(':');
+            var strLength = int.Parse(tempEncodeValue[..colonIndex]);
+
+            string tempString = tempEncodeValue.Substring(0, colonIndex + strLength + 1);
+            array.Add(StringBencode(tempString));
+
+            tempEncodeValue = tempEncodeValue.Substring(colonIndex + strLength + 1);
+        }
+        else if (tempEncodeValue[0] == 'i')
+        {
+            var endIndex = tempEncodeValue.IndexOf('e');
+            var numberLength = int.Parse(tempEncodeValue[1..endIndex]);
+
+            string tempString = tempEncodeValue.Substring(0, endIndex + 1);
+
+            array.Add(IntegerBencode(tempString));
+
+            tempEncodeValue = tempEncodeValue.Substring(endIndex + 1);
+        }
+        else if (tempEncodeValue[0] == 'l')
+        {
+            array.Add(ArrayBencode(tempEncodeValue.Substring(0, tempEncodeValue.LastIndexOf('e'))));
+            tempEncodeValue = tempEncodeValue.Substring(tempEncodeValue.LastIndexOf('e'));
+        }
+        Console.WriteLine(tempEncodeValue);
+        Console.WriteLine(JsonSerializer.Serialize(array));
+    }
+    return array;
 }
