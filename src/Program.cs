8de5ef036c4638e4e9710532e16adff25fb93a0f
DarKrone <Brener-2000@mail.ru>
2024-10-09 02:33:00 +0400

    FinalArrayDecode

--- src/Program.cs
+++ src/Program.cs
@@ -12,100 +12,92 @@ var (command, param) = args.Length switch
 if (command == "decode")
 {
     var encodedValue = param;
-    if (Char.IsDigit(encodedValue[0]))
-    {
-        Console.WriteLine(JsonSerializer.Serialize(StringBencode(encodedValue)));
-    }
-    else if (encodedValue[0] == 'i')
-    {
-        Console.WriteLine(JsonSerializer.Serialize(IntegerBencode(encodedValue)));
-    }
-    else if (encodedValue[0] == 'l')
-    {
-        
-        Console.WriteLine(JsonSerializer.Serialize(ArrayBencode(encodedValue)));
-    }
-    else
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
+    Console.WriteLine(JsonSerializer.Serialize(Bencode.Decode(encodedValue)));
 }
 else
 {
     throw new InvalidOperationException($"Invalid command: {command}");
 }
 
-string StringBencode(string encodedValue)
+public class Bencode()
 {
-    // Example: "5:hello" -> "hello"
-    var colonIndex = encodedValue.IndexOf(':');
-    if (colonIndex != -1)
+    public static object Decode(string input)
     {
-        var strLength = int.Parse(encodedValue[..colonIndex]);
-        var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-        return strValue;
-    }
-    else
-    {
-        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-    }
-}
-
-long IntegerBencode(string encodedValue)
-{
-    var endIndex = encodedValue.IndexOf('e');
-    if (endIndex != -1)
-    {
-        if (long.TryParse(encodedValue.Substring(1, endIndex - 1), out var number))
+        if (Char.IsDigit(input[0]))
         {
-            return number;
-        }   
+            return StringDecode(input);
+        }
+        else if (input[0] == 'i')
+        {
+            return IntegerDecode(input);
+        }
+        else if (input[0] == 'l')
+        {
+            return ArrayDecode(input);
+        }
         else
         {
-            throw new InvalidOperationException("Invalid number was given: " + number);
+            throw new InvalidOperationException("Unhandled encoded value: " + input);
         }
     }
-    else
+
+    public static string Encode(object input)
     {
-        throw new InvalidOperationException("Missing end of output (e)");
+        return Type.GetTypeCode(input.GetType()) switch
+        {
+            TypeCode.Int16 or TypeCode.Int32 or TypeCode.Int64 => $"i{input}e",
+            TypeCode.String => $"{((string)input).Length}:{input}",
+            TypeCode.Object => input is object[] inputArray ? $"l{string.Join("", inputArray.Select(x => Encode(x)))}e"
+                                                            : throw new Exception($"Unknown type: {input.GetType().FullName}"),
+            _ => throw new Exception($"Unknown type: {input.GetType().FullName}")
+        };
     }
-}
 
-List<object> ArrayBencode(string encodedValue)
-{
-    List<object> array = new List<object>();
-    string tempEncodeValue = encodedValue.Substring(1);
-    //Console.WriteLine(encodedValue);
-    while (tempEncodeValue != "e" || tempEncodeValue.Length == 0)
+    public static string StringDecode(string input)
     {
-        if (Char.IsDigit(tempEncodeValue[0]))
+        var colonIndex = input.IndexOf(':');
+        if (colonIndex != -1)
         {
-            var colonIndex = tempEncodeValue.IndexOf(':');
-            var strLength = int.Parse(tempEncodeValue[..colonIndex]);
-
-            string tempString = tempEncodeValue.Substring(0, colonIndex + strLength + 1);
-            array.Add(StringBencode(tempString));
-
-            tempEncodeValue = tempEncodeValue.Substring(colonIndex + strLength + 1);
+            var strLength = int.Parse(input[..colonIndex]);
+            var strValue = input.Substring(colonIndex + 1, strLength);
+            return strValue;
         }
-        else if (tempEncodeValue[0] == 'i')
+        else
         {
-            var endIndex = tempEncodeValue.IndexOf('e');
-            var numberLength = int.Parse(tempEncodeValue[1..endIndex]);
-
-            string tempString = tempEncodeValue.Substring(0, endIndex + 1);
-
-            array.Add(IntegerBencode(tempString));
-
-            tempEncodeValue = tempEncodeValue.Substring(endIndex + 1);
+            throw new InvalidOperationException("Invalid encoded value: " + input);
         }
-        else if (tempEncodeValue[0] == 'l')
-        {
-            array.Add(ArrayBencode(tempEncodeValue.Substring(0, tempEncodeValue.LastIndexOf('e'))));
-            tempEncodeValue = tempEncodeValue.Substring(tempEncodeValue.LastIndexOf('e'));
-        }
-        //Console.WriteLine(tempEncodeValue);
-        //Console.WriteLine(JsonSerializer.Serialize(array));
     }
-    return array;
+
+    public static long IntegerDecode(string input)
+    {
+        var endIndex = input.IndexOf('e');
+        if (endIndex != -1)
+        {
+            if (long.TryParse(input.Substring(1, endIndex - 1), out var number))
+            {
+                return number;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid number was given: " + number);
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Missing end of output (e)");
+        }
+    }
+
+    public static object[] ArrayDecode(string input)
+    {
+        input = input[1..];
+        var result = new List<object>();
+        while (input.Length > 0 && input[0] != 'e')
+        {
+            var element = Decode(input);
+            result.Add(element);
+            input = input[Encode(element).Length..];
+        }
+        return result.ToArray();
+    }
 }
