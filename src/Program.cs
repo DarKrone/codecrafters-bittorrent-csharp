357549fe87f92b4034e09fb519183cd865743ce6
DarKrone <Brener-2000@mail.ru>
2024-10-10 21:49:05 +0400

    DownloadFile

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,7 @@ using System.Security.Cryptography;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var command = args[0];
 
@@ -28,15 +28,15 @@ internal class Program
         }
         else if (command == "handshake")
         {
-            ShowPeerId(args[1], args[2]);
+            await ShowPeerId(args[1], args[2]);
         }
         else if (command == "download_piece")
         {
-            Download(args[1], args[2], args[3], int.Parse(args[4]));
+            await DownloadFile(args[1], args[2], args[3], int.Parse(args[4]));
         }
         else if (command == "download")
         {
-            Download(args[1], args[2], args[3], -1); // -1 for download all pieces
+            await DownloadFile(args[1], args[2], args[3], -1); // -1 for download all pieces
         }
         else
         {
@@ -74,7 +74,7 @@ internal class Program
         }
     }
 
-    private static async void ShowPeerId(string torrentFileName, string address)
+    private static async Task ShowPeerId(string torrentFileName, string address)
     {
         var tcpClient = new TcpClient();
         var addressAndPort = Address.GetAddressFromIPv4(address!);
@@ -87,7 +87,7 @@ internal class Program
         Console.WriteLine($"Peer ID: {peerID}");
     }
 
-    private static async void Download(string outputFlag, string fileLocation, string torrentFileName, int pieceIndex) // if need download all pieces: pieceIndex = -1
+    private static async Task DownloadFile(string outputFlag, string saveFileLocation, string torrentFileName, int pieceIndex) // if need download all pieces: pieceIndex = -1
     {
         MetaInfo metaInfo = MetaInfo.GetInfo(torrentFileName);
         var bytes = ReadWriteFile.ReadBytesFromFile(torrentFileName);
@@ -105,7 +105,7 @@ internal class Program
 
         var stream = tcpClient.GetStream();
         var peerID = HandShake.DoHandShake(torrentFileName, address!, stream).Result;
-        await codecrafters_bittorrent.src.Download.GetReadyToDownload(stream);
+        await Download.GetReadyToDownload(stream);
         if (pieceIndex == -1)
         {
             List<byte> combinedPieces = new List<byte>();
@@ -114,12 +114,12 @@ internal class Program
                 var filePiece = await codecrafters_bittorrent.src.Download.DownloadPiece(stream, i, metaInfo, pieceHashes[i]);
                 combinedPieces.AddRange(filePiece);
             }
-            ReadWriteFile.WriteBytesToFile(fileLocation, combinedPieces.ToArray());
+            ReadWriteFile.WriteBytesToFile(saveFileLocation, combinedPieces.ToArray());
         }
         else
         {
             var filePiece = await codecrafters_bittorrent.src.Download.DownloadPiece(stream, pieceIndex, metaInfo, pieceHashes[pieceIndex]);
-            ReadWriteFile.WriteBytesToFile(fileLocation, filePiece);
+            ReadWriteFile.WriteBytesToFile(saveFileLocation, filePiece);
         }
 
         tcpClient.Close();
