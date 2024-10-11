0ff30abf07399418c3ecbb9be5bf631252e72aad
DarKrone <Brener-2000@mail.ru>
2024-10-11 23:30:24 +0400

    WrongTest

--- src/HandShake.cs
+++ src/HandShake.cs
@@ -53,7 +53,7 @@ namespace codecrafters_bittorrent.src
             Dictionary<string, object> extensionMsg = new Dictionary<string, object>();
             Dictionary<string, object> extensions = new Dictionary<string, object>();
 
-            extensions.Add("ut_metadata", (byte)222);
+            extensions.Add("ut_metadata", 1);
             extensionMsg.Add("m", extensions);
 
             Console.WriteLine(JsonSerializer.Serialize(extensionMsg));
