31829b352332a438dd02ed7ece2998876ac21294
DarKrone <Brener-2000@mail.ru>
2024-10-12 18:53:54 +0400

    WrongTest

--- src/HandShake.cs
+++ src/HandShake.cs
@@ -68,18 +68,6 @@ namespace codecrafters_bittorrent.src
             handShakeMsg.Add((byte)msgId);
             handShakeMsg.Add((byte)0);
             handShakeMsg.AddRange(byteDict);
-            var buffer = new byte[128];
-            
-            while (true)
-            {
-                tcpStream.Read(buffer);
-
-                if (buffer[4] != 20)
-                {
-                    Console.WriteLine("Handshake received");
-                    break;
-                }
-            }
 
             Console.WriteLine("Sending message: ");
             foreach (var item in handShakeMsg)
@@ -92,6 +80,18 @@ namespace codecrafters_bittorrent.src
   
             Console.WriteLine("Handshake sended");
 
+            var buffer = new byte[128];
+            
+            while (true)
+            {
+                tcpStream.Read(buffer);
+
+                if (buffer[4] != 20)
+                {
+                    Console.WriteLine("Handshake received");
+                    break;
+                }
+            }
 
             return buffer;
         }
